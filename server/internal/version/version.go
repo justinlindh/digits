0da843453a768afd8e8dc498cae330cbec21da14
justinlindh <justinlindh@gmail.com>
2026-05-15 11:07:11 -0700

    docs(server): tighten version.go comment

--- server/internal/version/version.go
+++ server/internal/version/version.go
@@ -1,7 +1,7 @@
 // Package version provides build-time version info for the digits server.
 package version
 
-// These are set at build time via -ldflags.
+// Set at build time via -ldflags.
 // Example: go build -ldflags "-X .../version.Version=1.2.3 -X .../version.Commit=abc123"
 var (
 	Version = "dev"
